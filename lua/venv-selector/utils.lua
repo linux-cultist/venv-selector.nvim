5da3ea136751753ddff1381a741d28c7934ef1fc
Raúl Sánchez Ruiz <ulthuan@gmail.com>
2023-03-29 15:48:08 +0200

    fix(core): fix windows compatibility

--- lua/venv-selector/utils.lua
+++ lua/venv-selector/utils.lua
@@ -1,17 +1,22 @@
 local utils = {}
 
 utils.print_table = function(t)
-	print(vim.inspect(t))
+  print(vim.inspect(t))
 end
 
 utils.escape_pattern = function(text)
-	return text:gsub("([^%w])", "%%%1")
+  return text:gsub("([^%w])", "%%%1")
 end
 
 utils.remove_last_slash = function(s)
-	if string.sub(s, -1, -1) == "/" then
-		return string.sub(s, 1, -2)
-	end
+  local separator = "\\"
+  local sysname = vim.loop.os_uname().sysname
+  if sysname == "Linux" or sysname == "Darwin" then
+    separator = "/"
+  end
+  if string.sub(s, -1, -1) == separator then
+    return string.sub(s, 1, -2)
+  end
 end
 
 return utils

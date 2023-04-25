3ad721cdd9e9280402e7ad730b935087dbd60005
IceS2 <pjt1991@gmail.com>
2023-04-25 10:21:18 +0200

    Revert "fix: indentantion"
    This reverts commit e9a3574b28d138d20579c3476e9c965ec8b6417d.

--- lua/venv-selector/config.lua
+++ lua/venv-selector/config.lua
@@ -7,21 +7,21 @@ config = {
 
 -- Default settings if user is not setting anything in setup() call
 config.default_settings = {
-    search = true,
-    name = "venv",
-    search_workspace = true,
-    search_venv_managers = true,
-    parents = 2, -- When search is true, go this many directories up from the current opened buffer
-    poetry_path = system.get_venv_manager_default_path("Poetry"),
-    pipenv_path = system.get_venv_manager_default_path("Pipenv"),
-    pyenv_path = system.get_venv_manager_default_path("Pyenv"),
-    enable_debug_output = false,
-    auto_refresh = false, -- Uses cached results from last search
-    fd_binary_name = "fd",
-    cache_file = system.get_cache_default_path() .. "venvs.json",
-    cache_dir = system.get_cache_default_path(),
-    dap_enabled = false,
-    changed_venv_hooks = { hooks.pyright_hook, hooks.pylsp_hook },
+	search = true,
+	name = "venv",
+	search_workspace = true,
+	search_venv_managers = true,
+	parents = 2, -- When search is true, go this many directories up from the current opened buffer
+	poetry_path = system.get_venv_manager_default_path("Poetry"),
+	pipenv_path = system.get_venv_manager_default_path("Pipenv"),
+  pyenv_path = system.get_venv_manager_default_path("Pyenv"),
+	enable_debug_output = false,
+	auto_refresh = false, -- Uses cached results from last search
+	fd_binary_name = "fd",
+	cache_file = system.get_cache_default_path() .. "venvs.json",
+	cache_dir = system.get_cache_default_path(),
+	dap_enabled = false,
+	changed_venv_hooks = { hooks.pyright_hook, hooks.pylsp_hook },
 }
 
 -- Gets the search path supplied by the user in the setup function, or use current open buffer directory.

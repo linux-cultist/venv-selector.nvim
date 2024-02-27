57a0c92c0dd014d1e6305dc7124dc376c9e32e79
neolong <j31422210@gmail.com>
2024-02-27 23:54:43 +0800

    adjust pyright and pylance hook for compatibility

--- lua/venv-selector/hooks.lua
+++ lua/venv-selector/hooks.lua
@@ -32,8 +32,12 @@ end
 --- @type VenvChangedHook
 function M.pyright_hook(_, venv_python)
   M.execute_for_clients('pyright', function(client)
-    client.config.settings =
-      vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = venv_python } })
+    if client.settings then
+      client.settings = vim.tbl_deep_extend('force', client.settings, { python = { pythonPath = venv_python } })
+    else
+      client.config.settings =
+        vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = venv_python } })
+    end
     client.notify('workspace/didChangeConfiguration', { settings = nil })
   end)
 end
@@ -41,8 +45,12 @@ end
 --- @type VenvChangedHook
 function M.pylance_hook(_, venv_python)
   M.execute_for_clients('pylance', function(client)
-    client.config.settings =
-      vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = venv_python } })
+    if client.settings then
+      client.settings = vim.tbl_deep_extend('force', client.settings, { python = { pythonPath = venv_python } })
+    else
+      client.config.settings =
+        vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = venv_python } })
+    end
     client.notify('workspace/didChangeConfiguration', { settings = nil })
   end)
 end
@@ -50,7 +58,7 @@ end
 --- @type VenvChangedHook
 function M.pylsp_hook(_, venv_python)
   M.execute_for_client('pylsp', function(client)
-    local settings = vim.tbl_deep_extend('force', client.config.settings, {
+    local settings = vim.tbl_deep_extend('force', (client.settings or client.config.settings), {
       pylsp = {
         plugins = {
           jedi = {

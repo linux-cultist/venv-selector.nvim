d39c476fd34e01afbdfdf7506f61409a11c3acc0
linux_cultist <usual.beach6829@fastmail.com>
2024-03-16 12:53:28 +0100

    Regexp ideas

--- lua/venv-selector/init.lua
+++ lua/venv-selector/init.lua
@@ -1,64 +1,69 @@
-local venv = require 'venv-selector.venv'
-local config = require 'venv-selector.config'
-local dbg = require('venv-selector.utils').dbg
-local mytelescope = require 'venv-selector.mytelescope'
-local hooks = require 'venv-selector.hooks'
-local utils = require 'venv-selector.utils'
-
 local M = {}
 
--- Called by user when using the plugin.
-function M.setup(settings)
-  -- Let user config overwrite any default config options.
-  config.settings = vim.tbl_deep_extend('force', config.default_settings, settings or {})
-  dbg(config.settings)
+function M.printTable(tbl)
+  for _, part in ipairs(tbl) do
+    print(part)
+  end
+end
 
-  -- Create the VenvSelect command.
-  local venv_select_current = function()
-    if M.get_active_venv() ~= nil then
-      utils.notify("Activated '" .. (M.get_active_venv()) .. "'")
-    else
-      utils.notify 'No venv has been selected yet.'
+function M.splitArgs(args)
+  local t = {}
+  local text = args
+  local spat, epat, buf, quoted = [=[^(['"])]=], [=[(['"])$]=]
+  for str in text:gmatch '%S+' do
+    local squoted = str:match(spat)
+    local equoted = str:match(epat)
+    local escaped = str:match [=[(\*)['"]$]=]
+    if squoted and not quoted and not equoted then
+      buf, quoted = str, squoted
+    elseif buf and equoted == quoted and #escaped % 2 == 0 then
+      str, buf, quoted = buf .. ' ' .. str, nil, nil
+    elseif buf then
+      buf = buf .. ' ' .. str
+    end
+    if not buf then
+      table.insert(t, (str:gsub(spat, ''):gsub(epat, '')))
     end
   end
-
-  vim.api.nvim_create_user_command('VenvSelect', M.open, { desc = 'Activate venv' })
-  vim.api.nvim_create_user_command('VenvSelectCached', M.retrieve_from_cache, { desc = 'Retrieve venv from cache' })
-  vim.api.nvim_create_user_command('VenvSelectCurrent', venv_select_current, { desc = 'Show currently selected venv' })
-
-  -- Check if the user has the requirements to run VenvSelect
-  if utils.fd_or_fdfind_exists() == false then
-    utils.error "Missing requirement: VenvSelect needs 'fd' to be installed: https://github.com/sharkdp/fd."
+  if buf then
+    print('Missing matching quote for ' .. buf)
   end
+
+  return t
 end
 
--- Gets the system path to current active python in the venv (or nil if its not activated)
-function M.get_active_path()
-  return venv.current_python_path
+function M.filter(results)
+  local filtered = {}
+  for line in results:gmatch '[^\r\n]+' do
+    table.insert(filtered, line)
+  end
+
+  return filtered
 end
 
--- Gets the system path to the current active venv (or nil if its not activated)
-function M.get_active_venv()
-  return venv.current_venv
+function M.search(path)
+  local command = string.format('fd -HItd -E /proc --color never --absolute-path .venv$', path)
+  local result = vim.fn.system(command)
+  print(result)
+  return result
 end
 
--- The main function runs when user executes VenvSelect command
-function M.open()
-  mytelescope.open()
+function M.executeSearch(opts, settings)
+  settings.default_path = '/home/cado/Code/Blocket'
+  settings.search_func = M.search -- Let user specify his own search func
+  settings.filter_func = M.filter -- Let user specify his own filter func
+
+  local args = M.splitArgs(opts.args)
+  local path = args[1] or settings.default_path
+  local result = settings.search_func(path)
+  local filtered_result = settings.filter_func(result)
+  M.printTable(filtered_result)
 end
 
-function M.deactivate_venv()
-  venv.deactivate_venv()
+function M.setup(settings)
+  vim.api.nvim_create_user_command('VenvSelect', function(opts)
+    M.executeSearch(opts, settings)
+  end, { nargs = '*', desc = 'Activate venv' })
 end
-
-function M.retrieve_from_cache()
-  return venv.retrieve_from_cache()
-end
-
-M.hooks = {
-  pyright = hooks.pyright_hook,
-  pylance = hooks.pylance_hook,
-  pylsp = hooks.pylsp_hook,
-}
 
 return M

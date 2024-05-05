538a0c57ba4f451eff8f8f78b6f038b0ba82aa2d
linux_cultist <usual.beach6829@fastmail.com>
2024-05-05 18:35:50 +0200

    Revert "Changes"
    This reverts commit 471fe256ec49e0587af8011190fa89d76e5834d3.

---
+++ lua/venv-selector/workspace.lua
@@ -0,0 +1,17 @@
+M = {}
+
+function M.list_folders()
+    local workspace_folders = {}
+
+    for _, client in pairs((vim.lsp.get_clients or vim.lsp.get_active_clients)()) do
+        if vim.tbl_contains({ 'pyright', 'pylance' }, client.name) then
+            for _, folder in pairs(client.workspace_folders or {}) do
+                table.insert(workspace_folders, folder.name)
+            end
+        end
+    end
+
+    return workspace_folders
+end
+
+return M

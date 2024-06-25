81bde604348815d6c1bedefcf960e94a653c1ad6
barikata1984 <franche1984@gmail.com>
2024-06-25 00:46:42 -0400

    fix miniconda_envs/base

--- lua/venv-selector/gui.lua
+++ lua/venv-selector/gui.lua
@@ -8,250 +8,254 @@ local M = {}
 M.results = {}
 
 function M.insert_result(row)
-  log.debug 'Result:'
-  log.debug(row)
+    log.debug 'Result:'
+    log.debug(row)
 
-  table.insert(M.results, row)
-  M.update_results()
+    table.insert(M.results, row)
+    M.update_results()
 end
 
 function M.get_sorter()
-  local sorters = require 'telescope.sorters'
-  local conf = require('telescope.config').values
+    local sorters = require 'telescope.sorters'
+    local conf = require('telescope.config').values
 
-  local choices = {
-    ['character'] = function()
-      return conf.file_sorter()
-    end,
-    ['substring'] = function()
-      return sorters.get_substr_matcher()
-    end,
-  }
+    local choices = {
+        ['character'] = function()
+            return conf.file_sorter()
+        end,
+        ['substring'] = function()
+            return sorters.get_substr_matcher()
+        end,
+    }
 
-  return choices[config.user_settings.options.telescope_filter_type]
+    return choices[config.user_settings.options.telescope_filter_type]
 end
 
 function M.make_entry_maker()
-  local entry_display = require 'telescope.pickers.entry_display'
+    local entry_display = require 'telescope.pickers.entry_display'
 
-  local displayer = entry_display.create {
-    separator = ' ',
-    items = {
-      { width = 2 },
-      { width = 90 },
-      { width = 2 },
-      { width = 20 },
-      { width = 0.95 },
-    },
-  }
-
-  local function draw_icons_for_types(e)
-    if vim.tbl_contains({ 'cwd', 'workspace', 'file' }, e.source) then
-      return '󰥨'
-    elseif
-      vim.tbl_contains {
-        'virtualenvs',
-        'hatch',
-        'poetry',
-        'pyenv',
-        'anaconda_envs',
-        'anaconda_base',
-        'miniconda',
-        'miniconda',
-        'pipx',
-      }
-    then
-      return ''
-    else
-      return '' -- user created venv icon
-    end
-  end
-
-  local function hl_active_venv(e)
-    local icon_highlight = 'VenvSelectActiveVenv'
-    if e.path == path.current_python_path then
-      return icon_highlight
-    end
-    return nil
-  end
-
-  return function(entry)
-    local icon = entry.icon
-    entry.value = entry.name
-    entry.ordinal = entry.path
-    entry.display = function(e)
-      return displayer {
-        {
-          icon,
-          hl_active_venv(entry),
+    local displayer = entry_display.create {
+        separator = ' ',
+        items = {
+            { width = 2 },
+            { width = 90 },
+            { width = 2 },
+            { width = 20 },
+            { width = 0.95 },
         },
-        { e.name },
-        { config.user_settings.options.show_telescope_search_type and draw_icons_for_types(entry) or '' },
-        { config.user_settings.options.show_telescope_search_type and e.source or '' },
-      }
+    }
+
+    local function draw_icons_for_types(e)
+        if vim.tbl_contains({ 'cwd', 'workspace', 'file' }, e.source) then
+            return '󰥨'
+        elseif
+            vim.tbl_contains {
+                'virtualenvs',
+                'hatch',
+                'poetry',
+                'pyenv',
+                'anaconda_envs',
+                'anaconda_base',
+                'miniconda_envs',
+                'miniconda_base',
+                'pipx',
+            }
+        then
+            return ''
+        else
+            return '' -- user created venv icon
+        end
     end
 
-    return entry
-  end
+    local function hl_active_venv(e)
+        local icon_highlight = 'VenvSelectActiveVenv'
+        if e.path == path.current_python_path then
+            return icon_highlight
+        end
+        return nil
+    end
+
+    return function(entry)
+        local icon = entry.icon
+        entry.value = entry.name
+        entry.ordinal = entry.path
+        entry.display = function(e)
+            return displayer {
+                {
+                    icon,
+                    hl_active_venv(entry),
+                },
+                { e.name },
+                { config.user_settings.options.show_telescope_search_type and draw_icons_for_types(entry) or '' },
+                { config.user_settings.options.show_telescope_search_type and e.source or '' },
+            }
+        end
+
+        return entry
+    end
 end
 
 function M.remove_dups()
-  -- If a venv is found both by another search AND (cwd or file) search, then keep the one found by another search.
-  local seen = {}
-  local filtered_results = {}
+    -- If a venv is found both by another search AND (cwd or file) search, then keep the one found by another search.
+    local seen = {}
+    local filtered_results = {}
 
-  for _, v in ipairs(M.results) do
-    if not seen[v.name] then
-      seen[v.name] = v
-    else
-      local prev_entry = seen[v.name]
-      if (v.source == 'file' or v.source == 'cwd') and (prev_entry.source ~= 'file' and prev_entry.source ~= 'cwd') then
-        -- Current item has less priority, do not add it
-      elseif
-        (prev_entry.source == 'file' or prev_entry.source == 'cwd') and (v.source ~= 'file' and v.source ~= 'cwd')
-      then
-        -- Previous item has less priority, replace it
-        seen[v.name] = v
-      end
+    for _, v in ipairs(M.results) do
+        if not seen[v.name] then
+            seen[v.name] = v
+        else
+            local prev_entry = seen[v.name]
+            if
+                (v.source == 'file' or v.source == 'cwd')
+                and (prev_entry.source ~= 'file' and prev_entry.source ~= 'cwd')
+            then
+                -- Current item has less priority, do not add it
+            elseif
+                (prev_entry.source == 'file' or prev_entry.source == 'cwd')
+                and (v.source ~= 'file' and v.source ~= 'cwd')
+            then
+                -- Previous item has less priority, replace it
+                seen[v.name] = v
+            end
+        end
     end
-  end
 
-  for _, entry in pairs(seen) do
-    table.insert(filtered_results, entry)
-  end
+    for _, entry in pairs(seen) do
+        table.insert(filtered_results, entry)
+    end
 
-  M.results = filtered_results
+    M.results = filtered_results
 end
 
 function M.sort_results()
-  local selected_python = path.current_python_path
-  local current_file_dir = vim.fn.expand '%:p:h'
+    local selected_python = path.current_python_path
+    local current_file_dir = vim.fn.expand '%:p:h'
 
-  log.debug("Calculating path similarity based on: '" .. current_file_dir .. "'")
-  -- Normalize path by converting all separators to a common one (e.g., '/')
-  local function normalize_path(path)
-    return path:gsub('\\', '/')
-  end
-
-  -- Calculate the path similarity
-  local function path_similarity(path1, path2)
-    path1 = normalize_path(path1)
-    path2 = normalize_path(path2)
-    local segments1 = vim.split(path1, '/')
-    local segments2 = vim.split(path2, '/')
-    local count = 0
-    for i = 1, math.min(#segments1, #segments2) do
-      if segments1[i] == segments2[i] then
-        count = count + 1
-      else
-        break
-      end
-    end
-    return count
-  end
-
-  log.debug 'Sorting telescope results on path similarity.'
-  table.sort(M.results, function(a, b)
-    -- Check for 'selected_python' match
-    local a_is_selected = a.path == selected_python
-    local b_is_selected = b.path == selected_python
-    if a_is_selected and not b_is_selected then
-      return true
-    elseif not a_is_selected and b_is_selected then
-      return false
+    log.debug("Calculating path similarity based on: '" .. current_file_dir .. "'")
+    -- Normalize path by converting all separators to a common one (e.g., '/')
+    local function normalize_path(path)
+        return path:gsub('\\', '/')
     end
 
-    -- Compare based on path similarity
-    local sim_a = path_similarity(a.path, current_file_dir)
-    local sim_b = path_similarity(b.path, current_file_dir)
-    if sim_a ~= sim_b then
-      return sim_a > sim_b
+    -- Calculate the path similarity
+    local function path_similarity(path1, path2)
+        path1 = normalize_path(path1)
+        path2 = normalize_path(path2)
+        local segments1 = vim.split(path1, '/')
+        local segments2 = vim.split(path2, '/')
+        local count = 0
+        for i = 1, math.min(#segments1, #segments2) do
+            if segments1[i] == segments2[i] then
+                count = count + 1
+            else
+                break
+            end
+        end
+        return count
     end
 
-    -- Fallback to alphabetical sort
-    return a.name > b.name
-  end)
+    log.debug 'Sorting telescope results on path similarity.'
+    table.sort(M.results, function(a, b)
+        -- Check for 'selected_python' match
+        local a_is_selected = a.path == selected_python
+        local b_is_selected = b.path == selected_python
+        if a_is_selected and not b_is_selected then
+            return true
+        elseif not a_is_selected and b_is_selected then
+            return false
+        end
+
+        -- Compare based on path similarity
+        local sim_a = path_similarity(a.path, current_file_dir)
+        local sim_b = path_similarity(b.path, current_file_dir)
+        if sim_a ~= sim_b then
+            return sim_a > sim_b
+        end
+
+        -- Fallback to alphabetical sort
+        return a.name > b.name
+    end)
 end
 
 function M.update_results()
-  local finders = require 'telescope.finders'
-  local actions_state = require 'telescope.actions.state'
+    local finders = require 'telescope.finders'
+    local actions_state = require 'telescope.actions.state'
 
-  local finder = finders.new_table {
-    results = M.results,
-    entry_maker = M.make_entry_maker(),
-  }
+    local finder = finders.new_table {
+        results = M.results,
+        entry_maker = M.make_entry_maker(),
+    }
 
-  local bufnr = vim.api.nvim_get_current_buf()
-  local picker = actions_state.get_current_picker(bufnr)
-  if picker ~= nil then
-    picker:refresh(finder, { reset_prompt = false })
-  end
+    local bufnr = vim.api.nvim_get_current_buf()
+    local picker = actions_state.get_current_picker(bufnr)
+    if picker ~= nil then
+        picker:refresh(finder, { reset_prompt = false })
+    end
 end
 
 function M.open(in_progress)
-  local finders = require 'telescope.finders'
-  local pickers = require 'telescope.pickers'
-  local actions_state = require 'telescope.actions.state'
-  local actions = require 'telescope.actions'
+    local finders = require 'telescope.finders'
+    local pickers = require 'telescope.pickers'
+    local actions_state = require 'telescope.actions.state'
+    local actions = require 'telescope.actions'
 
-  local title = 'Virtual environments (ctrl-r to refresh)'
+    local title = 'Virtual environments (ctrl-r to refresh)'
 
-  if in_progress == false then
-    M.sort_results()
-  end
+    if in_progress == false then
+        M.sort_results()
+    end
 
-  local finder = finders.new_table {
-    results = M.results,
-    entry_maker = M.make_entry_maker(),
-  }
+    local finder = finders.new_table {
+        results = M.results,
+        entry_maker = M.make_entry_maker(),
+    }
 
-  local opts = {
-    prompt_title = title,
-    finder = finder,
-    layout_strategy = 'vertical',
-    layout_config = {
-      height = 0.4,
-      width = 120,
-      prompt_position = 'top',
-    },
-    cwd = require('telescope.utils').buffer_dir(),
+    local opts = {
+        prompt_title = title,
+        finder = finder,
+        layout_strategy = 'vertical',
+        layout_config = {
+            height = 0.4,
+            width = 120,
+            prompt_position = 'top',
+        },
+        cwd = require('telescope.utils').buffer_dir(),
 
-    sorting_strategy = 'ascending',
-    sorter = M.get_sorter()(),
-    attach_mappings = function(bufnr, map)
-      map('i', '<cr>', function()
-        local selected_entry = actions_state.get_selected_entry()
-        local activated = false
-        if selected_entry ~= nil then
-          activated = venv.activate(config.user_settings.hooks, selected_entry)
-          if activated == true then
-            path.add(path.get_base(selected_entry.path))
-            path.update_python_dap(selected_entry.path)
-            path.save_selected_python(selected_entry.path)
+        sorting_strategy = 'ascending',
+        sorter = M.get_sorter()(),
+        attach_mappings = function(bufnr, map)
+            map('i', '<cr>', function()
+                local selected_entry = actions_state.get_selected_entry()
+                local activated = false
+                if selected_entry ~= nil then
+                    activated = venv.activate(config.user_settings.hooks, selected_entry)
+                    if activated == true then
+                        path.add(path.get_base(selected_entry.path))
+                        path.update_python_dap(selected_entry.path)
+                        path.save_selected_python(selected_entry.path)
 
-            if selected_entry.type == 'anaconda' then
-              venv.unset_env 'VIRTUAL_ENV'
-              venv.set_env(selected_entry.path, 'CONDA_PREFIX')
-            else
-              venv.unset_env 'CONDA_PREFIX'
-              venv.set_env(selected_entry.path, 'VIRTUAL_ENV')
-            end
-          end
-        end
-        actions.close(bufnr)
-      end)
+                        if selected_entry.type == 'anaconda' then
+                            venv.unset_env 'VIRTUAL_ENV'
+                            venv.set_env(selected_entry.path, 'CONDA_PREFIX')
+                        else
+                            venv.unset_env 'CONDA_PREFIX'
+                            venv.set_env(selected_entry.path, 'VIRTUAL_ENV')
+                        end
+                    end
+                end
+                actions.close(bufnr)
+            end)
 
-      map('i', '<C-r>', function()
-        M.results = {}
-        local search = require 'venv-selector.search'
-        search.New(nil)
-      end)
+            map('i', '<C-r>', function()
+                M.results = {}
+                local search = require 'venv-selector.search'
+                search.New(nil)
+            end)
 
-      return true
-    end,
-  }
-  pickers.new({}, opts):find()
+            return true
+        end,
+    }
+    pickers.new({}, opts):find()
 end
 
 return M
--- lua/venv-selector/venv.lua
+++ lua/venv-selector/venv.lua
@@ -7,106 +7,107 @@ local M = {}
 M.current_source = nil -- contains the name of the search, like anaconda, pipx etc.
 
 function M.stop_lsp_servers()
-  local hooks = require('venv-selector.config').user_settings.hooks
-  for _, hook in pairs(hooks) do
-    hook(nil)
-  end
+    local hooks = require('venv-selector.config').user_settings.hooks
+    for _, hook in pairs(hooks) do
+        hook(nil)
+    end
 end
 
 function M.activate(hooks, selected_entry)
-  local python_path = selected_entry.path
-  local venv_type = selected_entry.type
-  local source = selected_entry.source
-  local on_venv_activate_callback = config.user_settings.options.on_venv_activate_callback
+    local python_path = selected_entry.path
+    local venv_type = selected_entry.type
+    local source = selected_entry.source
+    local on_venv_activate_callback = config.user_settings.options.on_venv_activate_callback
 
-  if python_path ~= nil then
-    log.debug('Telescope entry selected by user: ', selected_entry)
-    local count = 0
-    for _, hook in pairs(hooks) do
-      count = count + hook(python_path)
-    end
+    if python_path ~= nil then
+        log.debug('Telescope entry selected by user: ', selected_entry)
+        local count = 0
+        for _, hook in pairs(hooks) do
+            count = count + hook(python_path)
+        end
 
-    if count == 0 then
-      local message = 'No python lsp servers are running. Please open a python file and then select a venv to activate.'
-      vim.notify(message, vim.log.levels.INFO, { title = 'VenvSelect' })
-      log.info(message)
-      return false
-    else
-      local cache = require 'venv-selector.cached_venv'
-      cache.save(python_path, venv_type, source)
-      if on_venv_activate_callback ~= nil then
-        M.current_source = source
-        log.debug('Setting require("venv-selector").source() to \'' .. source .. "'")
-        log.debug 'Calling on_venv_activate_callback() function'
-        on_venv_activate_callback()
-      end
-      return true
+        if count == 0 then
+            local message =
+                'No python lsp servers are running. Please open a python file and then select a venv to activate.'
+            vim.notify(message, vim.log.levels.INFO, { title = 'VenvSelect' })
+            log.info(message)
+            return false
+        else
+            local cache = require 'venv-selector.cached_venv'
+            cache.save(python_path, venv_type, source)
+            if on_venv_activate_callback ~= nil then
+                M.current_source = source
+                log.debug('Setting require("venv-selector").source() to \'' .. source .. "'")
+                log.debug 'Calling on_venv_activate_callback() function'
+                on_venv_activate_callback()
+            end
+            return true
+        end
     end
-  end
 end
 
 function M.activate_from_cache(settings, venv_info)
-  log.debug 'Activating venv from cache'
+    log.debug 'Activating venv from cache'
 
-  -- Set the below two variables as quick as possible since its used in sorting results in telescope
-  -- and if the user is quick to open the telescope before lsp has activated, the selected
-  -- venv wont be displayed otherwise.
-  path.current_python_path = venv_info.value
-  path.current_venv_path = path.get_base(venv_info.value)
+    -- Set the below two variables as quick as possible since its used in sorting results in telescope
+    -- and if the user is quick to open the telescope before lsp has activated, the selected
+    -- venv wont be displayed otherwise.
+    path.current_python_path = venv_info.value
+    path.current_venv_path = path.get_base(venv_info.value)
 
-  local venv = require 'venv-selector.venv'
-  local python_path = venv_info.value
-  local venv_type = venv_info.type
-  local venv_source = venv_info.source
-  local on_venv_activate_callback = config.user_settings.options.on_venv_activate_callback
-  for _, hook in pairs(settings.hooks) do
-    hook(python_path)
-  end
+    local venv = require 'venv-selector.venv'
+    local python_path = venv_info.value
+    local venv_type = venv_info.type
+    local venv_source = venv_info.source
+    local on_venv_activate_callback = config.user_settings.options.on_venv_activate_callback
+    for _, hook in pairs(settings.hooks) do
+        hook(python_path)
+    end
 
-  local is_conda_family = venv_type == 'anaconda' or venv_type == 'miniconda'
+    local is_conda_family = venv_type == 'anaconda' or venv_type == 'miniconda'
 
-  if venv_type ~= nil and is_conda_family then -- venv_type == 'anaconda' then
-    venv.unset_env 'VIRTUAL_ENV'
-    venv.set_env(python_path, 'CONDA_PREFIX')
-  else
-    venv.unset_env 'CONDA_PREFIX'
-    venv.set_env(python_path, 'VIRTUAL_ENV')
-  end
+    if venv_type ~= nil and is_conda_family then -- venv_type == 'anaconda' then
+        venv.unset_env 'VIRTUAL_ENV'
+        venv.set_env(python_path, 'CONDA_PREFIX')
+    else
+        venv.unset_env 'CONDA_PREFIX'
+        venv.set_env(python_path, 'VIRTUAL_ENV')
+    end
 
-  path.update_python_dap(python_path)
-  path.save_selected_python(python_path)
-  path.add(path.get_base(python_path))
+    path.update_python_dap(python_path)
+    path.save_selected_python(python_path)
+    path.add(path.get_base(python_path))
 
-  if on_venv_activate_callback ~= nil then
-    M.current_source = venv_source
-    log.debug('Setting require("venv-selector").source() to \'' .. venv_source .. '"')
-    log.debug 'Calling on_venv_activate_callback() function'
-    on_venv_activate_callback()
-  end
+    if on_venv_activate_callback ~= nil then
+        M.current_source = venv_source
+        log.debug('Setting require("venv-selector").source() to \'' .. venv_source .. '"')
+        log.debug 'Calling on_venv_activate_callback() function'
+        on_venv_activate_callback()
+    end
 end
 
 function M.set_env(python_path, env_variable_name)
-  if config.user_settings.options.set_environment_variables == true then
-    local env_path = path.get_base(path.get_base(python_path))
-    if env_path ~= nil then
-      vim.fn.setenv(env_variable_name, env_path)
-      log.debug('$' .. env_variable_name .. ' set to ' .. env_path)
+    if config.user_settings.options.set_environment_variables == true then
+        local env_path = path.get_base(path.get_base(python_path))
+        if env_path ~= nil then
+            vim.fn.setenv(env_variable_name, env_path)
+            log.debug('$' .. env_variable_name .. ' set to ' .. env_path)
+        end
     end
-  end
 end
 
 function M.unset_env(env_variable_name)
-  if config.user_settings.options.set_environment_variables == true then
-    if vim.fn.getenv(env_variable_name) ~= nil then
-      vim.fn.setenv(env_variable_name, nil)
-      log.debug('$' .. env_variable_name .. ' has been unset.')
+    if config.user_settings.options.set_environment_variables == true then
+        if vim.fn.getenv(env_variable_name) ~= nil then
+            vim.fn.setenv(env_variable_name, nil)
+            log.debug('$' .. env_variable_name .. ' has been unset.')
+        end
     end
-  end
 end
 
 function M.unset_env_variables()
-  vim.fn.setenv('VIRTUAL_ENV', nil)
-  vim.fn.setenv('CONDA_PREFIX', nil)
+    vim.fn.setenv('VIRTUAL_ENV', nil)
+    vim.fn.setenv('CONDA_PREFIX', nil)
 end
 
 return M

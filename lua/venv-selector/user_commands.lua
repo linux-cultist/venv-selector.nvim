0c3c32e6a7c6045fc1ff14e03389a4a91251de22
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-03 20:09:55 -0800

    refactor: restructure gui module and extract telescope picker to its own file
    This is to make it easier to add new pickers
    Additionally, the entrypoint of search has changed from `search.New` to
    `gui.open`, because `run_search` is now passed a picker object

--- lua/venv-selector/user_commands.lua
+++ lua/venv-selector/user_commands.lua
@@ -1,5 +1,4 @@
 local config = require("venv-selector.config")
-local search = require("venv-selector.search")
 local gui = require("venv-selector.gui")
 local log = require("venv-selector.logger")
 
@@ -7,8 +6,7 @@ local M = {}
 
 function M.register()
     vim.api.nvim_create_user_command("VenvSelect", function(opts)
-        gui.open(search.search_in_progress)
-        search.New(opts)
+        gui.open(opts)
     end, { nargs = "*", desc = "Activate venv" })
 
     vim.api.nvim_create_user_command("VenvSelectLog", function()

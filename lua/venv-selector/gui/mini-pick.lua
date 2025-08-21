c4a8fcd7bed1d9e7b8856b09f56adba7abf26882
Pavel Kirilin <win10@list.ru>
2025-08-21 21:44:26 +0200

    Finally. Mini pick integration.

--- lua/venv-selector/gui/mini-pick.lua
+++ lua/venv-selector/gui/mini-pick.lua
@@ -5,6 +5,8 @@ M.__index = M
 
 local H = {}
 
+-- Create a namespace for extmarks
+-- This is used to highlight the active virtual environment in the results
 H.ns_id = vim.api.nvim_create_namespace("MiniPickVenvSelect")
 
 function M.new()
@@ -22,11 +24,13 @@ function M:search_done()
 	self.results = gui_utils.remove_dups(self.results)
 	gui_utils.sort_results(self.results)
 
-	-- TODO: is there any way to add color to the results?
 	mini_pick.start({
 		source = {
+			-- Name of the source, used for display purposes
 			name = "Virtual environments",
+			-- List of virtual environments
 			items = self.results,
+			-- Function to preview venvs
 			preview = function(buf_id, item)
 				local lines = {
 					"Source: " .. item.source,
@@ -41,31 +45,34 @@ function M:search_done()
 						table.insert(lines, line)
 					end
 				end
+				-- Update the buffer with the preview content
 				vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
 			end,
+			-- Function for rendering list of venvs
 			show = function(buf_id, items_arr, query)
 				local lines = {}
+				-- Format each item as a string
 				for _, item in ipairs(items_arr) do
 					table.insert(lines, gui_utils.format_result_as_string(item.icon, item.source, item.name))
 				end
+				-- Set the buffer lines to the formatted items
 				vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
-				-- -- Remove previous extmarks
+				-- -- Remove previous highlight extmarks
 				pcall(vim.api.nvim_buf_clear_namespace, buf_id, H.ns_id, 0, -1)
 				-- Add new extmarks for icons
 				for i, item in ipairs(items_arr) do
+					-- Check if the item should be highlighted
 					local hl = gui_utils.hl_active_venv(item)
 					if hl ~= nil then
-						local succ, err = pcall(vim.api.nvim_buf_set_extmark, buf_id, H.ns_id, i - 1, 0, {
+						-- Highlight the icon in the first column
+						pcall(vim.api.nvim_buf_set_extmark, buf_id, H.ns_id, i - 1, 0, {
 							end_col = 1,
 							hl_group = hl,
-							priority = 200,
 						})
-						if not succ then
-							print("Error setting extmark: " .. err)
-						end
 					end
 				end
 			end,
+			-- Function to handle selection of a venv
 			choose = function(item)
 				gui_utils.select(item)
 			end,
